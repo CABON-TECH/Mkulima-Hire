d605b5da41e9a7d9dcaea7776e315af74c1d79fc
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-05-24 18:41:45 +0300

    created an update api

--- server/controllers/farmerController.js
+++ server/controllers/farmerController.js
@@ -45,11 +45,27 @@ const getFarmerById = async (req, res) => {
     }
   };
 
+  const updateFarmer = async (req, res) => {
+    try {
+      const farmer = await Farmer.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      if (!farmer) {
+        return res.status(404).json({ error: 'Farmer not found' });
+      }
+      res.json(farmer);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+
 
 
 
     module.exports = {
         getAllFarmers,
         createFarmer,
-        getFarmerById
+        getFarmerById,
+        updateFarmer
     }
--- server/routes/farmerRoute.js
+++ server/routes/farmerRoute.js
@@ -6,5 +6,6 @@ const farmerController = require('../controllers/farmerController');
 router.get('/', farmerController.getAllFarmers);
 router.post('/', farmerController.createFarmer);
 router.get('/:id', farmerController.getFarmerById);
+router.put('/:id', farmerController.updateFarmer);
 
 module.exports = router;
