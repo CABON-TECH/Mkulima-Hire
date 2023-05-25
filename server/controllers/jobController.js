dbdda2f43d08a390bb77dc959d96c73ca0b36709
cabon-tech <cabon-tech@cabon-tech.cabon-tech>
2023-05-26 00:43:12 +0300

    created an updatejobbyid api

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -38,10 +38,33 @@ const getJobById = async (req, res, next) => {
     }
 };
 
+// update a job by id
+const updateJobById = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const {title, description, city, state, phone, email, pay} = req.body;
+
+        const updatedJob = await Job.findByIdAndUpdate(id, {title, description, city, state, phone, email, pay}, {new: true});
+
+        if (!updatedJob) {
+            return next(new ErrorResponse('Job not found', 404));
+        }
+
+        res.status(200).json({
+            success: true,
+            data: updatedJob
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+
 module.exports = {
     getAllJobs,
     createJob,
-    getJobById
+    getJobById,
+    updateJobById
 };
 
 
--- server/routes/jobRoute.js
+++ server/routes/jobRoute.js
@@ -4,6 +4,7 @@ const jobController = require('../controllers/jobController');
 router.get('/', jobController.getAllJobs);
 router.post('/', jobController.createJob);
 router.get('/:id', jobController.getJobById);
+router.put('/:id', jobController.updateJobById);
 
 
 module.exports = router;
