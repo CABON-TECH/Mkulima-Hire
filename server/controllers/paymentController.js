bc3de4ac923c2a2f57df4b967b4788ba5bda4642
cabon-tech <cabon-tech@cabon-tech.cabon-tech>
2023-05-26 01:24:05 +0300

    created an api for create payment

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,8 +1,8 @@
 const Payment = require('../models/paymentModel');
 
-const paymentController = {};
+//const paymentController = {};
 
-paymentController.getAllPayments = async (req, res) => {
+const getAllPayments = async (req, res) => {
     try {
         const payments = await Payment.find({});
         res.status(200).json({
@@ -18,4 +18,30 @@ paymentController.getAllPayments = async (req, res) => {
     }
 }
 
-module.exports = paymentController;
+// create payment
+const createPayment = async (req, res) => {
+    try {
+        const { user, onModel, amount, paymentMethod, paymentResult } = req.body;
+        const payment = await Payment.create({
+            user,
+            onModel,
+            amount,
+            paymentMethod,
+            paymentResult
+        });
+        res.status(201).json({
+            status: 'success',
+            payment
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        });
+    }
+}
+
+module.exports = {
+    getAllPayments,
+    createPayment
+}
--- server/routes/paymentRoute.js
+++ server/routes/paymentRoute.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const paymentController = require('../controllers/paymentController');
 
 router.get('/', paymentController.getAllPayments);
+router.post('/', paymentController.createPayment);
 
 module.exports = router;
