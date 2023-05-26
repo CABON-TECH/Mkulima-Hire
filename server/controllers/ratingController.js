f26f7913febb538d7d905518063859231d48ed5c
cabon-tech <cabon-tech@cabon-tech.cabon-tech>
2023-05-26 14:37:57 +0300

    created a put api for rating

--- server/controllers/ratingController.js
+++ server/controllers/ratingController.js
@@ -30,84 +30,149 @@ const createRating = asyncHandler(async (req, res) => {
 );
 
 //get all ratings of a farmer
-const getFarmerRatings = asyncHandler(async (req, res) => {
-    const farmer = await Farmer.findById(req.params.id);
-    if (farmer) {
+//const getFarmerRatings = asyncHandler(async (req, res) => {
+  //  const farmer = await Farmer.findById(req.params.id);
+    //if (farmer) {
 
-        const ratings = await Rating.find({ onModel: 'Farmer', user: req.params.id });
-        res.status(200).json({
-            status: 'success',
-            results: ratings.length,
-            ratings
-        });
-    } else {
-        res.status(404);
-        throw new Error('Farmer not found');
-    }
-}
-);
+      //  const ratings = await Rating.find({ onModel: 'Farmer', user: req.params.id });
+        //res.status(200).json({
+          //  status: 'success',
+            //results: ratings.length,
+            //ratings
+        //});
+    //} else {
+      //  res.status(404);
+        //throw new Error('Farmer not found');
+    //}
+//}
+//);
 
 //get all ratings of a worker
-const getWorkerRatings = asyncHandler(async (req, res) => {
-    const worker = await Worker.findById(req.params.id);
-    if (worker) {
+//const getWorkerRatings = asyncHandler(async (req, res) => {
+  //  const worker = await Worker.findById(req.params.id);
+    //if (worker) {
 
-        const ratings = await Rating.find({ onModel: 'Worker', user: req.params.id });
-        res.status(200).json({
-            status: 'success',
-            results: ratings.length,
-            ratings
-        });
-    } else {
-        res.status(404);
-        throw new Error('Worker not found');
-    }
-}
-);
+      //  const ratings = await Rating.find({ onModel: 'Worker', user: req.params.id });
+        //res.status(200).json({
+          //  status: 'success',
+            //results: ratings.length,
+            //ratings
+        //});
+    //} else {
+      //  res.status(404);
+        //throw new Error('Worker not found');
+    //}
+//}
+//);
 
 //get average rating of a farmer
-const getFarmerAverageRating = asyncHandler(async (req, res) => {
-    const farmer = await Farmer.findById(req.params.id);
-    if (farmer) {
+//const getFarmerAverageRating = asyncHandler(async (req, res) => {
+  //  const farmer = await Farmer.findById(req.params.id);
+    //if (farmer) {
 
-        const ratings = await Rating.find({ onModel: 'Farmer', user: req.params.id });
-        let sum = 0;
-        for (let i = 0; i < ratings.length; i++) {
-            sum += ratings[i].rating;
-        }
-        const averageRating = sum / ratings.length;
-        res.status(200).json({
-            status: 'success',
-            averageRating
-        });
-    } else {
-        res.status(404);
-        throw new Error('Farmer not found');
-    }
-}
-);
+      //  const ratings = await Rating.find({ onModel: 'Farmer', user: req.params.id });
+        //let sum = 0;
+        //for (let i = 0; i < ratings.length; i++) {
+          //  sum += ratings[i].rating;
+        //}
+        //const averageRating = sum / ratings.length;
+        //res.status(200).json({
+          //  status: 'success',
+            //averageRating
+        //});
+    //} else {
+      //  res.status(404);
+        //throw new Error('Farmer not found');
+    //}
+//}
+//);
 
 //get average rating of a worker
-const getWorkerAverageRating = asyncHandler(async (req, res) => {
-    const worker = await Worker.findById(req.params.id);
-    if (worker) {
+//const getWorkerAverageRating = asyncHandler(async (req, res) => {
+  //  const worker = await Worker.findById(req.params.id);
+    //if (worker) {
 
-        const ratings = await Rating.find({ onModel: 'Worker', user: req.params.id });
-        let sum = 0;
-        for (let i = 0; i < ratings.length; i++) {
-            sum += ratings[i].rating;
-        }
-        const averageRating = sum / ratings.length;
+      //  const ratings = await Rating.find({ onModel: 'Worker', user: req.params.id });
+        //let sum = 0;
+        //for (let i = 0; i < ratings.length; i++) {
+          //  sum += ratings[i].rating;
+        //}
+        //const averageRating = sum / ratings.length;
+        //res.status(200).json({
+          //  status: 'success',
+            //averageRating
+        //});
+    //} else {
+      //  res.status(404);
+        //throw new Error('Worker not found');
+    //}
+//}
+//);
+
+//get total rating of a farmer
+//const getFarmerTotalRating = asyncHandler(async (req, res) => {
+  //  const farmer = await Farmer.findById(req.params.id);
+    //if (farmer) {
+
+      //  const ratings = await Rating.find({ onModel: 'Farmer', user: req.params.id });
+        //let sum = 0;
+        //for (let i = 0; i < ratings.length; i++) {
+          //  sum += ratings[i].rating;
+        //}
+        //res.status(200).json({
+          //  status: 'success',
+            //totalRating: sum
+        //});
+    //} else {
+      //  res.status(404);
+        //throw new Error('Farmer not found');
+    //}
+//}
+//);
+
+//get total rating of a worker
+//const getWorkerTotalRating = asyncHandler(async (req, res) => {
+  //  const worker = await Worker.findById(req.params.id);
+    //if (worker) {
+
+      //  const ratings = await Rating.find({ onModel: 'Worker', user: req.params.id });
+        //let sum = 0;
+        //for (let i = 0; i < ratings.length; i++) {
+          //  sum += ratings[i].rating;
+        //}
+        //res.status(200).json({
+          //  status: 'success',
+            //totalRating: sum
+        //});
+    //} else {
+      //  res.status(404);
+        //throw new Error('Worker not found');
+    //}
+//}
+//);
+
+
+
+
+//update rating
+const updateRating = asyncHandler(async (req, res) => {
+    const rating = await Rating.findById(req.params.id);
+    if (rating) {
+        rating.rating = req.body.rating || rating.rating;
+        rating.comment = req.body.comment || rating.comment;
+        const updatedRating = await rating.save();
         res.status(200).json({
             status: 'success',
-            averageRating
+            updatedRating
         });
     } else {
         res.status(404);
-        throw new Error('Worker not found');
+        throw new Error('Rating not found');
     }
 }
 );
+
+
 
 
 
@@ -118,7 +183,10 @@ module.exports = {
     getFarmerRatings,
     getWorkerRatings,
     getFarmerAverageRating,
-    getWorkerAverageRating
+    getWorkerAverageRating,
+    getFarmerTotalRating,
+    getWorkerTotalRating,
+    updateRating,
     
 }
 
--- server/routes/ratingRoute.js
+++ server/routes/ratingRoute.js
@@ -3,8 +3,14 @@ const ratingController = require('../controllers/ratingController');
 
 router.get('/', ratingController.getAllRatings);
 router.post('/', ratingController.createRating);
-router.get('/farmer/:id', ratingController.getFarmerRatings);
-router.get('/worker/:id', ratingController.getWorkerRatings);
+router.get('/:id', ratingController.getFarmerRatings);
+//router.get('/worker/:id', ratingController.getWorkerRatings);
+//router.get('/farmer/:id/average', ratingController.getFarmerAverageRating);
+//router.get('/worker/:id/average', ratingController.getWorkerAverageRating);
+//router.get('/farmer/:id/total', ratingController.getFarmerTotalRating);
+//router.get('/worker/:id/total', ratingController.getWorkerTotalRating);
+router.put('/:id', ratingController.updateRating);
+
 
 
 module.exports = router;
