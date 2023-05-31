51f174b6bb4cbc2473c4558ac68ba1b10e8d98b7
cabon-tech <cabon-tech@cabon-tech.cabon-tech>
2023-05-31 18:39:37 +0300

    created a login api

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -37,13 +37,40 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Invalid user data');
     }
-
-
-
-    
-
-
-    
 });
 
-module.exports = { registerUser };
+    //login user after registration
+    const loginUser = asyncHandler(async (req, res) => {
+
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            res.json({
+              _id: user.id,
+              name: user.name,
+              email: user.email,
+              //token: generateToken(user._id),
+            })
+          } else {
+            res.status(400)
+            throw new Error('Invalid credentials')
+          }
+        
+    })
+
+
+
+
+
+
+    
+
+
+    
+
+
+module.exports = {
+    registerUser,
+    loginUser
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser } = require('../controllers/userController');
-
+const { registerUser, loginUser} = require('../controllers/userController');
 router.route('/').post(registerUser);
+router.route('/login').post(loginUser);
 
 
 module.exports = router;
