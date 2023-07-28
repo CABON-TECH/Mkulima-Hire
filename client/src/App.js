8ad8f66ce2ef5d6bce7e5d256262b252cba3f718
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-07-28 20:05:53 +0300

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-//import Footer from "./components/Footer"
+
+import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
-//import Home from "./pages/Home"
+import Home from "./pages/Home"
+
 
 
 function App() {
   return (
     <div>
       <Navbar />
+      
+      <Home />
+      
+      
+      <Footer />
       
       
     </div>
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
 
-function Footer() {
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import { Box } from "@mui/material";
+
+export default function Footer() {
   return (
-    <div>
-    <h1>footer</h1>
-    </div>
-  )
+    <Box
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[200]
+            : theme.palette.grey[800],
+        p: 6,
+      }}
+      component="footer"
+    >
+      <Container maxWidth="sm">
+        <Typography variant="body2" color="text.secondary" align="center">
+          {"Copyright © "}
+          <Link color="inherit" href="https://your-website.com/">
+            Your Website
+          </Link>{" "}
+          {new Date().getFullYear()}
+          {"."}
+        </Typography>
+      </Container>
+    </Box>
+  );
 }
-
-export default Footer
