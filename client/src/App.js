34af317383436d480d3ff3d1f4f61510b1141cc0
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-07-28 12:51:11 +0300

    navbar

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+//import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+//import Home from "./pages/Home"
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      
+      
+    </div>
+  )
+}
+
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
+    
   </React.StrictMode>
 );
 
