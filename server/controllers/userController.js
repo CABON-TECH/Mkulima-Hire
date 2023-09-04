1bc046a78fa8791b5df2e6a65d276d1e2eef5e92
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-09-04 20:03:12 +0300

    updated user role

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -42,6 +42,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user.name,
             email: user.email,
+            role: user.role,
             token: generateToken(user._id),
         });
     } else{
